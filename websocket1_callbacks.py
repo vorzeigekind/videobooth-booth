4716c96a8f4bb2b6a01ee4992d01270c36bd7a3a
VorzeigeNiklas <143099489+VorzeigeNiklas@users.noreply.github.com>
2023-08-29 10:15:02 +0200

    Create websocket1_callbacks.py

---
+++ websocket1_callbacks.py
@@ -0,0 +1,73 @@
+# me - this DAT
+# dat - the WebSocket DAT
+import json
+
+def onConnect(dat):
+	print('connected')
+	return
+
+# me - this DAT
+# dat - the WebSocket DAT
+
+def onDisconnect(dat):
+	print('disconnected')
+	return
+
+# me - this DAT
+# dat - the DAT that received a message
+# rowIndex - the row number the message was placed into
+# message - a unicode representation of the text
+# 
+# Only text frame messages will be handled in this function.
+
+def onReceiveText(dat, rowIndex, message):
+	if message == 'ping':
+		dat.sendText('pong')
+		return
+	data = json.loads(message)
+	for key, val in data.items():
+		mod('table_modifier').insertVal(key, val)
+		# write the key values into a table
+		print(key, val)
+	return
+
+
+# me - this DAT
+# dat - the DAT that received a message
+# contents - a byte array of the message contents
+# 
+# Only binary frame messages will be handled in this function.
+
+def onReceiveBinary(dat, contents):
+	return
+
+# me - this DAT
+# dat - the DAT that received a message
+# contents - a byte array of the message contents
+# 
+# Only ping messages will be handled in this function.
+
+def onReceivePing(dat, contents):
+	dat.sendPong(contents) # send a reply with same message
+	return
+
+# me - this DAT
+# dat - the DAT that received a message
+# contents - a byte array of the message content
+# 
+# Only pong messages will be handled in this function.
+
+def onReceivePong(dat, contents):
+	return
+
+
+# me - this DAT
+# dat - the DAT that received a message
+# message - a unicode representation of the message
+#
+# Use this method to monitor the websocket status messages
+
+def onMonitorMessage(dat, message):
+	return
+
+	
