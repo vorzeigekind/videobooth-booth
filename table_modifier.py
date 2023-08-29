190a02f6eb91d65f759cd1065cb36464a7f632da
VorzeigeNiklas <143099489+VorzeigeNiklas@users.noreply.github.com>
2023-08-29 10:23:07 +0200

    Update table_modifier.py

--- table_modifier.py
+++ table_modifier.py
@@ -1,3 +1,5 @@
+#websocket nachrichten werden zur weiterverarbeitung in programmiernodes(operator) in eine touchdesigner tabelle eingetragen und gegebenenfalls aktualisiert oder neu angelegt.
+
 t = op('table1')
 
 def insertVal(key, val):
